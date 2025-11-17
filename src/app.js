15b49f4c8f105eb34ee7af62fa2904401f6b59e6
Manojes <manoj.esw@gmail.com>
2025-11-17 22:07:07 +0530

    playing with routing

--- src/app.js
+++ src/app.js
@@ -2,13 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.use("/manoj", (req, res) => {
-  res.send("hello from server manoj");
+
+app.get("/hello", (req, res) => {
+  res.send("get from server hello");
 });
 
-app.use((req, res) => {
-  res.send("hellod froh server");
+
+app.use("/hello", (req, res) => {
+  res.send("hello from server hello");
 });
+
+
+
+// app.use("/hello/1", (req, res) => {
+//   res.send("new");S
+// });
+
+// app.use("/manoj", (req, res) => {
+//   res.send("hello from server manoj");
+// });
+
+// app.use((req, res) => {
+//   res.send("hellod froh server");
+// });
+
+app.get("/user",()=>{
+
+})
 
 app.listen(3000, () => {
   console.log("server successfully started");
